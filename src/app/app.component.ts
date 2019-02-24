420019c1a9d0834c7059922f8523055f0b25cebf
sudeepkumar118 <sudeep.kumar17@gmail.com>
2019-02-24 21:28:48 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  public ngOnInit(){
+    $(document).ready(function(){
+
+      $(".filter-button").click(function(){
+          var value = $(this).attr('data-filter');
+          
+          if(value == "all")
+          {
+              //$('.filter').removeClass('hidden');
+              $('.filter').show('1000');
+          }
+          else
+          {
+      //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+      //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+              $(".filter").not('.'+value).hide('3000');
+              $('.filter').filter('.'+value).show('3000');
+              
+          }
+      });
+      
+      if ($(".filter-button").removeClass("active")) {
+      $(this).removeClass("active");
+      }
+      $(this).addClass("active");
+      
+      });
+  }
 }
+
---
+++ src/app/mainmenu.directive.spec.ts
@@ -0,0 +1,8 @@
+import { MainmenuDirective } from './mainmenu.directive';
+
+describe('MainmenuDirective', () => {
+  it('should create an instance', () => {
+    const directive = new MainmenuDirective();
+    expect(directive).toBeTruthy();
+  });
+});
